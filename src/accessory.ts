519c1d076e5802747f5afcc187748f3fdd1fd8f3
Romain DUBOC <romain@duboc.pro>
2021-04-25 20:12:59 +0200

    Add stateful option

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,20 +1,19 @@
 import {
-  AccessoryConfig,
-  AccessoryPlugin,
-  API,
-  CharacteristicEventTypes,
-  CharacteristicGetCallback,
-  CharacteristicSetCallback,
-  CharacteristicValue,
-  HAP,
-  Logging,
-  Service
-} from "homebridge";
+    AccessoryConfig,
+    AccessoryPlugin,
+    API,
+    Characteristic,
+    CharacteristicValue,
+    HAP,
+    Logging,
+    Service,
+} from 'homebridge';
 
 /*
  * IMPORTANT NOTICE
  *
- * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
+ * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module
+ * (or the "hap-nodejs" module).
  * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
  * and will disappear once the code is compiled to Javascript.
  * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
@@ -39,82 +38,100 @@ let hap: HAP;
  * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
-  hap = api.hap;
-  api.registerAccessory("Presence", Presence);
+    hap = api.hap;
+    api.registerAccessory('Presence', Presence);
 };
 
 class Presence implements AccessoryPlugin {
 
-  private readonly log: Logging;
-  private readonly name: string;
-  private readonly motionDelay: number;
-  private readonly occupancyDelay: number;
-  private motionTimer: NodeJS.Timeout|null = null;
-  private occupancyTimer: NodeJS.Timeout|null = null;
+    private readonly log: Logging;
+    private readonly name: string;
+    private readonly stateful: boolean;
 
-  private readonly informationService: Service;
-  private readonly switchService: Service;
-  private readonly motionService: Service;
-  private readonly occupancyService: Service;
+    private readonly services: Array<Service>;
 
-  constructor(log: Logging, config: AccessoryConfig, api: API) {
-    this.log = log;
-    this.name = config.name;
-    this.motionDelay = config.motionDelay || (60 * 60);
-    this.occupancyDelay = config.occupancyDelay || (12 * 60 * 60);
+    private readonly motionDelay: number;
+    private readonly occupancyDelay: number;
 
-    this.switchService = new hap.Service.Switch(this.name, 'Trigger');
-    this.motionService = new hap.Service.MotionSensor(this.name, 'Motion');
-    this.occupancyService = new hap.Service.OccupancySensor(this.name);
+    private motionTimer: NodeJS.Timeout | null = null;
+    private occupancyTimer: NodeJS.Timeout | null = null;
 
-    this.switchService.getCharacteristic(hap.Characteristic.On)
-      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        this.motionService.getCharacteristic(hap.Characteristic.MotionDetected).updateValue(true);
-        if(this.motionTimer != null) {
-          clearTimeout(this.motionTimer);
+    private readonly trigger: Characteristic;
+    private readonly motion: Characteristic;
+    private readonly presence: Characteristic;
+
+    constructor(log: Logging, config: AccessoryConfig, api: API) {
+        this.log = log;
+        this.name = config.name;
+        this.stateful = config.stateful || false;
+        this.motionDelay = config.motionDelay || (60 * 60);
+        this.occupancyDelay = config.occupancyDelay || (12 * 60 * 60);
+
+        const triggerService = new hap.Service.Switch(this.name, 'Trigger');
+        const motionService = new hap.Service.MotionSensor(this.name, 'Motion');
+        const occupancyService = new hap.Service.OccupancySensor(this.name);
+
+        this.trigger = triggerService.getCharacteristic(hap.Characteristic.On);
+        this.motion = motionService.getCharacteristic(hap.Characteristic.MotionDetected);
+        this.presence = occupancyService.getCharacteristic(hap.Characteristic.OccupancyDetected);
+
+        triggerService.getCharacteristic(hap.Characteristic.On).onSet(this.onTrigger.bind(this));
+
+        const informationService = new hap.Service.AccessoryInformation()
+            .setCharacteristic(hap.Characteristic.Manufacturer, 'github.com/dubocr')
+            .setCharacteristic(hap.Characteristic.Model, 'Presence');
+
+        log.info('Presence service initialized!');
+
+        this.services = [
+            informationService,
+            triggerService,
+            motionService,
+            occupancyService,
+        ];
+    }
+
+    private onTrigger(value: CharacteristicValue) {
+        if (value) {
+            this.motion.updateValue(true);
+            this.presence.updateValue(true);
+
+            if (this.motionTimer !== null) {
+                clearTimeout(this.motionTimer);
+            }
+
+            if (this.occupancyTimer !== null) {
+                clearTimeout(this.occupancyTimer);
+            }
+
+            if (!this.stateful) {
+                setTimeout(() => this.trigger.setValue(false), 1000);
+            }
+        } else {
+            this.motionTimer = setTimeout(
+                () => this.motion.updateValue(false),
+                (this.motionDelay * 1000),
+            );
+            this.occupancyTimer = setTimeout(
+                () => this.presence.updateValue(false),
+                (this.occupancyDelay * 1000),
+            );
         }
-        this.motionTimer = setTimeout(
-          () => this.motionService.getCharacteristic(hap.Characteristic.MotionDetected).updateValue(false),
-          (this.motionDelay * 1000)
-        );
+    }
 
-        this.occupancyService.getCharacteristic(hap.Characteristic.OccupancyDetected).updateValue(true);
-        if(this.occupancyTimer != null) {
-          clearTimeout(this.occupancyTimer);
-        }
-        this.occupancyTimer = setTimeout(
-          () => this.occupancyService.getCharacteristic(hap.Characteristic.OccupancyDetected).updateValue(false),
-          (this.occupancyDelay * 1000)
-        );
-        setTimeout(() => this.switchService.getCharacteristic(hap.Characteristic.On).updateValue(false), (5000));
-        callback();
-      });
+    /*
+     * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+     * Typical this only ever happens at the pairing process.
+     */
+    identify(): void {
+        this.log('Identify !');
+    }
 
-    this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, "github.com/dubocr")
-      .setCharacteristic(hap.Characteristic.Model, "Presence");
-
-    log.info("Presence service initialized!");
-  }
-
-  /*
-   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
-   * Typical this only ever happens at the pairing process.
-   */
-  identify(): void {
-    this.log("Identify!");
-  }
-
-  /*
-   * This method is called directly after creation of this instance.
-   * It should return all services which should be added to the accessory.
-   */
-  getServices(): Service[] {
-    return [
-      this.informationService,
-      this.switchService,
-      this.motionService,
-      this.occupancyService,
-    ];
-  }
+    /*
+     * This method is called directly after creation of this instance.
+     * It should return all services which should be added to the accessory.
+     */
+    getServices(): Service[] {
+        return this.services;
+    }
 }
